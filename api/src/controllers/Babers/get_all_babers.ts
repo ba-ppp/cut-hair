0b259783e5ba7ed50bb65c9eec5788af9c2d079f
khangb1910652 <93603484+khangb1910652@users.noreply.github.com>
2021-11-05 23:04:40 +0700

    baber

--- api/src/controllers/Babers/get_all_babers.ts
+++ api/src/controllers/Babers/get_all_babers.ts
@@ -1,13 +1,23 @@
 import express from "express";
+import { connection } from "../../database/mysql";
+
 const router = express.Router();
 
-
 export const get_all_babers = () => {
-  return router.post(
+  return router.get(
     "/",
     async (req: express.Request, res: express.Response) => {
       try {
-        res.json();
+        
+        const sql = "SELECT * FROM baber";
+        connection.query(sql, function (err, results) {
+          if (err) throw err;
+          res.send(results);
+        });
+
+        // sql
+        // const baber = {};
+        // res.json(baber);
       } catch (error) {
         res.json({
           status: 400,
