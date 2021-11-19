567a53940009803b94df169c991db9b4e63507a8
khangb1910652 <93603484+khangb1910652@users.noreply.github.com>
2021-11-19 22:45:38 +0700

    bill

--- api/src/controllers/bill/insertUpdateBill.ts
+++ api/src/controllers/bill/insertUpdateBill.ts
@@ -1,3 +1,4 @@
+
 import express from "express";
 import { connection } from "../../database/mysql";
 import {
@@ -22,21 +23,59 @@ export const insertBill = async (
   const sql1 = "call insertUpdateBill (?,?,?,?)";
   const sql2 = "call insertUpdateBillListService(?,?)";
   const sql3 = "call insertUpdateBillListProduct(?,?)";
-  connection.query(sql, [
-    customer.idCustomer,
-    customer.name,
-    customer.time,
-    customer.phone,
-  ]);
-  connection.query(sql1, [idBill, idBaber, customer.idCustomer]);
-  idSelectedServices.forEach((id) => {
-    connection.query(sql2, [idBill, id]);
-  });
-  idSelectedProducts.forEach((id) => {
-    connection.query(sql3, [idBill, id]);
-  });
+  try {
+    const rs = await new Promise((resolve, reject) => {
+      connection.query(
+        sql,
+        [customer.idCustomer, customer.name, customer.time, customer.phone],
+        (err) => {
+          if (err) reject(err);
+          resolve(true);
+        }
+      );
+    });
+    if (rs) {
+      const rs1 = await new Promise((resolve, reject) => {
+        connection.query(
+          sql1,
+          [idBill, idBaber, customer.idCustomer],
+          (err) => {
+            if (err) reject(err);
+            resolve(true);
+          }
+        );
+      });
+      if (rs1) {
+        const rs2 = await Promise.all(
+          idSelectedServices.map((id) => {
+            return new Promise((resolve, reject) => {
+              connection.query(sql2, [idBill, id], (err) => {
+                if (err) reject(err);
+                resolve(true);
+              });
+            });
+          })
+        );
+        const rs3 = await Promise.all(
+          idSelectedProducts.map((id) => {
+            return new Promise((resolve, reject) => {
+              connection.query(sql3, [idBill, id], (err) => {
+                if (err) reject(err);
+                resolve(true);
+              });
+            });
+          })
+        );
+        if (rs2 && rs3) {
+          return true;
+        }
+      }
+    }
+    return false;
+  } catch (error) {
+    return false;
+  }
 };
-
 
 export const insertUpdateBill = () => {
   return router.post(
@@ -53,8 +92,7 @@ export const insertUpdateBill = () => {
           idProductItem
         );
 
-        status.then((result: any) => {
-          
+        status.then((result) => {
           if (result) {
             res.json({
               status: 200,
